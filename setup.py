a610fa0a731906dd347c8c77af8423aa5bec30f4
Alec Edgington <54802828+cqc-alec@users.noreply.github.com>
2024-01-08 10:27:54 +0000

    Correct URL for documentation. (#55)

--- setup.py
+++ setup.py
@@ -32,7 +32,7 @@ setup(
     author_email="tket-support@cambridgequantum.com",
     python_requires=">=3.9",
     project_urls={
-        "Documentation": "https://tket.quantinuum.com/extensions/pytket-projectq/api/index.html",
+        "Documentation": "https://tket.quantinuum.com/extensions/pytket-projectq/index.html",
         "Source": "https://github.com/CQCL/pytket-projectq",
         "Tracker": "https://github.com/CQCL/pytket-projectq/issues",
     },
