c1f75103d0119c174274d34df610f458685c9db9
Melf <cqc@melf.de>
2025-03-05 16:57:44 +0000

    run black, fix format

--- pytket/extensions/projectq/backends/projectq_backend.py
+++ pytket/extensions/projectq/backends/projectq_backend.py
@@ -12,8 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-"""Methods to allow tket circuits to be ran on ProjectQ simulator
-"""
+"""Methods to allow tket circuits to be ran on ProjectQ simulator"""
 
 from typing import (
     Any,
--- pytket/extensions/projectq/projectq_convert.py
+++ pytket/extensions/projectq/projectq_convert.py
@@ -12,8 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-"""Methods to allow conversion between ProjectQ and tket data types
-"""
+"""Methods to allow conversion between ProjectQ and tket data types"""
 
 from typing import Any, Iterable
 
